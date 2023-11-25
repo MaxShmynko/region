d2b53c918d2c3269b4ceb3dd3a3f3c46bda5f95c
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-11-25 22:11:28 +0300

    add fantastic page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,34 @@
 import $ from 'jquery';
+import slick from "slick-carousel";
+import WOW from "wowjs";
 
 $(() => {
+  const wow = new WOW.WOW({
+		live: false,
+		boxClass: "wow",
+	});
 
+	wow.init();
+
+    $('.fantastic__wrap__list').slick({
+        infinite: true, 
+        slidesToShow: 1, 
+        slidesToScroll: 1,
+        prevArrow: $('.fantastic__wrap__button-prev'),
+        nextArrow: $('.fantastic__wrap__button-next'),
+    });
+
+    $('.fantastic__list__card-wrap-item-button').click(function () {
+        $('.fantastic__list__card-back').removeClass('flip-front');
+        $('.fantastic__list__card-front').removeClass('flip-back');
+        $('.fantastic__list__card-back').addClass('flip-back');
+        $('.fantastic__list__card-front').addClass('flip-front');
+    });
+
+    $('.fantastic__list__card-back-close').click(function () {
+        $('.fantastic__list__card-back').removeClass('flip-back');
+        $('.fantastic__list__card-front').removeClass('flip-front');
+        $('.fantastic__list__card-back').addClass('flip-front');
+        $('.fantastic__list__card-front').addClass('flip-back');
+    });
 });
